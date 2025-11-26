009b79ed85ce785033d59d957b71c362d975883c
Ryan Hull <hull.ryanc@gmail.com>
2025-11-26 00:27:55 -0500

    fix: extract full translated text by joining all segments from the Google Translate API response.

--- src/lib/utils/translation_service.js
+++ src/lib/utils/translation_service.js
@@ -10,8 +10,9 @@ export async function translateText(text, targetLang, sourceLang = 'auto') {
         const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLang}&tl=${targetLang}&dt=t&q=${encodeURIComponent(text)}`;
         const res = await fetch(url);
         const data = await res.json();
-        // data[0][0][0] usually contains the translated text
-        return data[0][0][0];
+        // data[0] contains an array of translated segments (sentences)
+        // We need to join them all together
+        return data[0].map(item => item[0]).join('');
     } catch (e) {
         console.error('Translation failed', e);
         const errorMessage = e instanceof Error ? e.message : String(e);
